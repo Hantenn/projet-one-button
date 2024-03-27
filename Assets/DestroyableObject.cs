1f26b09556aa58cf36cdd9d119b62381e961322b
Hantenn <147702055+Hantenn@users.noreply.github.com>
2024-03-28 00:15:24 +0100

    Update DestroyableObject.cs

--- Assets/DestroyableObject.cs
+++ Assets/DestroyableObject.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class DestroyableObject : MonoBehaviour
 {
     public GameObject plat;
+    public GameObject detruit;
     public void Start()
     {
         plat.SetActive(false);
     }
+    private OnTriggerEnter
     private void OnDestroy()
     {
         plat.SetActive(true);
